6f43d5e0337154e2f4dc44ec270df286aca3c9ea
dylan gebel <dylanfoxx@optonline.net>
2017-04-20 23:44:49 -0400

    fixed error with bez drawing and cleaned up code

--- reference/bezoldglsdl/bezwithsdl.c
+++ reference/bezoldglsdl/bezwithsdl.c
@@ -11,13 +11,13 @@ int main(int argc, char **argv)
 {
 
 	signal(SIGINT, signal_callback_handler);
-	
+
 	if(SDL_Init(SDL_INIT_VIDEO) != 0){
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,"ERROR-init",SDL_GetError(),NULL);
 		return 1;
 	}
-	
-	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);	
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
 	SDL_Window *window;
 	SDL_GLContext context;
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
 
 	if(window == NULL){
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,"ERROR-window",SDL_GetError(),NULL);
-		return 1;	
+		return 1;
 	}
 
 	context = SDL_GL_CreateContext(window);
@@ -41,7 +41,7 @@ int main(int argc, char **argv)
 
 	if(SDL_GL_SetSwapInterval(1) != 0){
 		SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR,"ERROR-interval",SDL_GetError(),NULL);
-		return 1;	
+		return 1;
 	}
 
 	 SDL_ShowCursor(SDL_DISABLE);
@@ -55,17 +55,17 @@ int main(int argc, char **argv)
 	SDL_GL_DeleteContext(context);
 
 	SDL_DestroyWindow(window);
-	
+
 	SDL_Quit();
 
 	return 0;
 }
 
 	void display(SDL_Window *winLoc)
-	{			
+	{
 
 		drawPatch();
-	
+
 		SDL_GL_SwapWindow(winLoc);
 
 		SDL_Delay(waitTime);
