e359430dbe3b5e3cf9111f804edc0a4da591a655
dylan gebel <dylanfoxx@optonline.net>
2017-05-04 14:42:31 -0400

    fixed merge errors

--- reference/assimptst/assimp.cpp
+++ reference/assimptst/assimp.cpp
@@ -17,9 +17,9 @@ const struct aiScene *scene = NULL;
 
 int main()
 {
-  //Assimp::Importer importer;//importer.ReadFile("boxy2.ply",aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_JoinIdenticalVertices);
+  //Assimp::Importer importer; //importer.ReadFile("boxy2.ply", aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_JoinIdenticalVertices);
 
-  scene = aiImportFile("boxy2.ply",aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_JoinIdenticalVertices);//imports file by name with proceesses anded
+  scene = aiImportFile("boxy2.ply", aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_JoinIdenticalVertices); //imports file by name with processes anded
   if (scene == NULL)
   {
     printf("ERROR: file not read\n");
@@ -32,21 +32,22 @@ int main()
 		return -1;
 	}
 
-  printf("%u meshes\n",scene->mNumMeshes);
+  printf("%u meshes\n", scene->mNumMeshes);
 
-  printf("%u materials\n",scene->mNumMaterials);
+  printf("%u materials\n", scene->mNumMaterials);
 
   for (size_t i = 0; i < scene->mNumMaterials; i++)
   {
-    printf("%u textruees\n", scene->mMaterials[i]->GetTextureCount((aiTextureType)1));
+    printf("%u textures\n", scene->mMaterials[i]->GetTextureCount((aiTextureType)1));
   }
 
   for (size_t i = 0; i < scene->mNumMeshes; i++)
   {
-    printf("verticies %u\n", scene->mMeshes[i]->mNumVertices);
-    printf("faces %u\n",scene->mMeshes[i]->mNumFaces);
+    printf("vertices %u\n", scene->mMeshes[i]->mNumVertices);
+    printf("faces %u\n", scene->mMeshes[i]->mNumFaces);
   }
 
+	float testNum = 69.000000;
   for (size_t i = 0; i < scene->mNumMeshes; i++)
   {
     for (size_t j = 0; j < scene->mMeshes[i]->mNumFaces; j++)
@@ -65,16 +66,25 @@ int main()
       y = scene->mMeshes[i]->mVertices[(scene->mMeshes[i]->mFaces[j].mIndices[2])].y;
       z = scene->mMeshes[i]->mVertices[(scene->mMeshes[i]->mFaces[j].mIndices[2])].z;
       printf("points x:%f,y:%f,z:%f\n", x,y,z);
+    	
+      //if ((i == 0) && (j == 0)) scene->mMeshes[i]->mFaces[j].mIndices[0] = testNum;
+      
+      //scene->mMeshes[i]->mFaces[j].mIndices[0] = testNum;
+      
+      float x = scene->mMeshes[i]->mFaces[j].mIndices[0];
+      float y = scene->mMeshes[i]->mFaces[j].mIndices[1];
+      float z = scene->mMeshes[i]->mFaces[j].mIndices[2];
+      printf("points x:%f, y:%f, z:%f\n", x, y, z);
     }
   }
 
-  size_t exportFormats = aiGetExportFormatCount();// number of supported export formats
-  std::cout << exportFormats << std::endl;
+  size_t exportFormats = aiGetExportFormatCount(); //number of supported export formats
+  //std::cout << exportFormats << std::endl;
 
   for (size_t i = 0; i < exportFormats; i++)
   {
     const aiExportFormatDesc *exportFormatsDesc;
-    exportFormatsDesc = aiGetExportFormatDescription(i);//gets description of what format
+    exportFormatsDesc = aiGetExportFormatDescription(i); //gets description of what format
 
     if (exportFormatsDesc == NULL)
     {
@@ -82,10 +92,10 @@ int main()
       exit(-1);
     }
 
-    std::cout << i << " " <<exportFormatsDesc->description <<" ID: " <<exportFormatsDesc->id <<  '\n';
-}
+    //std::cout << i << " " << exportFormatsDesc->description <<" ID: " << exportFormatsDesc->id <<  '\n';
+	}
 
-  aiExportScene(scene,"ply","Testexp.ply",aiProcess_SplitLargeMeshes);
+  aiExportScene (scene, "ply", "Testexp.ply", aiProcess_SplitLargeMeshes);
 
   const struct aiScene *scene2 = new aiScene();
   std::cout << scene2->HasMeshes() << std::endl;
